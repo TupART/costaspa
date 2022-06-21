5c68d3745861f358d8bb2bb726d3292abf3ae1f0
Gerard Escolies <gerard.esma@icloud.com>
2022-06-21 15:29:41 +0200

    first commit

---
+++ pages/horas/selectUser.jsx
@@ -0,0 +1,8 @@
+export default function SelectUser() {
+    return (
+    <div className="w-full flex flex-col items-center justify-center">
+        <div className="w-1/2 w-full rounded-md shadow-sm"> hola </div>
+        <div className="w-1/2 w-full rounded-md shadow-sm">hola </div>
+        <div className="w-1/2 w-full rounded-md shadow-sm">hola </div>
+    </div>)
+}
