ca6c6c6a567d613173257519ea80d91cfc7607b2
Gerard Escolies <gerard.esma@icloud.com>
2022-09-06 16:57:32 +0200

    fixing menu on darkmode

--- components/menu-link.jsx
+++ components/menu-link.jsx
@@ -1,11 +1,15 @@
-import { Link, Text } from '@nextui-org/react'
+import { Link, Text, useTheme } from '@nextui-org/react'
+
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 
 export default function MenuLink({ href, children }) {
   const { pathname } = useRouter()
+  const { isDark } = useTheme()
   const isActive = pathname.startsWith(href)
-  const css = isActive ? { color: '$primary' } : { color: '$black' }
+  const css = isActive
+    ? { color: '$primary' }
+    : { color: isDark ? '$white' : '$black' }
   return (
     <NextLink href={href}>
       <Text weight={isActive ? 'bold' : 'normal'}>
